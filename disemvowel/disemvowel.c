3c78baa6522e36fd61989268ce28fbb08a16221b
Rocherno De Jongh <dejon076@normandy.morris.umn.edu>
2017-09-28 11:11:56 -0500

    finished disemvowel.c, all of the tests in valgrind passes, script still has leaks, need to add comments to disemvowel.c too

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
-  return (char*) "";
+  int len, i, beg;
+  len = 0;
+  char *result;
+  char string[] = "aeiouAEIOU";
+  beg = strlen(str);
+
+  for (i=0; i<beg; ++i) {
+	if(strchr(string, str[i]) == NULL) {
+	  ++len;
+	 }
+  }
+
+  result = (char*) calloc(len+1, sizeof(char));
+
+  len = 0;
+
+  for (i=0; i<beg; ++i){
+	if(strchr(string, str[i]) == NULL){
+	  result[len] = str[i];
+	  ++len;
+	}
+  }
+
+  return result;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -10,7 +10,13 @@ int main(int argc, char* argv[]) {
   size = 100;
   line = (char*) malloc (size + 1);
 
+  char* result;
+
   while (getline(&line, &size, stdin) > 0) {
+    result = disemvowel(line);  
     printf("%s\n", disemvowel(line));
+    free(result);
   }
+
+  free(line);
 }
